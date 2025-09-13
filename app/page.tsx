28ac589413e703e2d91bd67a2412f98f0b210c8c
HitishRaoP <hitishraop@gmail.com>
2025-09-13 14:13:57 +0530

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import { HeaderMain } from "@/modules/header/header-main";
 import { HeroMain } from "@/modules/hero/hero-main";
-import { StatsBand } from "@/modules/stats/stats-band";
-import { WorksGrid } from "@/modules/works/works-grid";
-import { ServicesGrid } from "@/modules/services/services-grid";
 import { PricingSection } from "@/modules/pricing/pricing-section";
-import { PartnersRow } from "@/modules/partners/partners-row";
-import { TestimonialsList } from "@/modules/testimonials/testimonials-list";
-import { BlogList } from "@/modules/blog/blog-list";
 import { FooterMain } from "@/modules/footer/footer-main";
 
 export default function Home() {
@@ -14,13 +8,7 @@ export default function Home() {
     <div className="font-sans">
       <HeaderMain />
       <HeroMain />
-      <StatsBand />
-      <WorksGrid />
-      <ServicesGrid />
       <PricingSection />
-      <PartnersRow />
-      <TestimonialsList />
-      <BlogList />
       <FooterMain />
     </div>
   );
